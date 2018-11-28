5d4eb6a9be2ae4db12ac70a595a4aa48cd27573e
indah <indahrahmawati883@gmail.com>
2018-11-28 15:05:07 +0700

    commit ujian

--- exam.py
+++ exam.py
@@ -1,1 +1,55 @@
-def(menu):
+##SOAL NO 1
+
+# def nbYear(p0, percent, aug, p):
+#     return p
+#     for i in range()
+
+
+##SOAL NO 2
+# def tickets(peopleInLine) :
+
+    
+#     if(uang == 50 and uang == 100 ):
+#         print('yes')
+#     else :
+#         print('no')
+
+
+# listKembali = ["25,25,50"]
+# listHarga = { "film Avenger": 25}
+# listCart = { }
+    
+
+        
+
+
+##SOAL NO 3
+
+def rowSumOddNumbers(n): 
+      
+    num = 1
+      
+    for i in range(0, n): 
+        
+        for j in range(0, i+1): 
+            
+            
+            print(num, end= "  ")
+            
+            num = num + 2
+      
+        print("\r")   
+n = 5  
+rowSumOddNumbers(n) 
+
+Sum = sum(rowSumOddNumbers(n)) 
+print(Sum) 
+   
+    
+            
+
+
+
+
+    
+
