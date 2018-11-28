1407637a171507ed138c115ff6306926d7f7d54b
indah <indahrahmawati883@gmail.com>
2018-11-28 15:58:26 +0700

    commit ujian

--- exam.py
+++ exam.py
@@ -1,8 +1,14 @@
 ##SOAL NO 1
 
 # def nbYear(p0, percent, aug, p):
-#     return p
-#     for i in range()
+# text = "";
+# p0 = 1000;
+
+
+#  if(nbYear >= 1200):
+#     text = " Sudah Cukup";
+#  else :
+#     text = "masih sedikit";
 
 
 ##SOAL NO 2
@@ -24,32 +30,34 @@
 
 
 ##SOAL NO 3
+# def rowSumOddNumbers(n): 
+  
+#     num = 1
+#     for i in range(0, n): 
+         
+#         for j in range(0, i+1): 
+            
+#             print(num, end= "  ")
+            
+#             num = num + 2
+      
+#         print("\r") 
+       
+          
+# n = 5
+  
+# rowSumOddNumbers(n) 
 
-def rowSumOddNumbers(n): 
-      
-    num = 1
-      
-    for i in range(0, n): 
-        
-        for j in range(0, i+1): 
-            
-            
-            print(num, end= "  ")
-            
-            num = num + 2
-      
-        print("\r")   
-n = 5  
-rowSumOddNumbers(n) 
-
-Sum = sum(rowSumOddNumbers(n)) 
-print(Sum) 
-   
+# def sum(tri, m, n): 
+  
     
-            
-
-
-
-
-    
+#     for i in range(m-1, -1, -1): 
+#         for j in range(i+1):   
+#             if (tri[i+1][j] > tri[i+1][j+1]): 
+#                 tri[i][j] += tri[i+1][j] 
+#             else: 
+#                 tri[i][j] += tri[i+1][j+1] 
+     
+#     return tri[0][0] 
+  
 
